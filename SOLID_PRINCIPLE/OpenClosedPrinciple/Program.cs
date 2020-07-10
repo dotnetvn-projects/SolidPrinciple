80b6893b84b10bfbeeba83e69223df6d04d452d8
Linh Pham <duylinh191@gmail.com>
2020-07-11 00:29:19 +0700

    Update

--- SOLID_PRINCIPLE/OpenClosedPrinciple/Program.cs
+++ SOLID_PRINCIPLE/OpenClosedPrinciple/Program.cs
@@ -9,38 +9,38 @@ namespace OpenClosedPrinciple
     class Program
     {
         // Before applying Open-Closed Principle
-        static void Main(string[] args)
-        {
-            var fileItems = CreateData();
-
-            Before.FileFilter fileFilter = new Before.FileFilter();
-
-            var imageFiles = fileFilter.FilterByType(fileItems, FileType.Image);
-            Console.WriteLine("Image Files: {0}", string.Join(", ", imageFiles.Select(c => c.FileName)));
-
-            var latestFiles = fileFilter.FilterByName(fileItems, "File_3");
-            Console.WriteLine("Latest Files: {0}", string.Join(", ", latestFiles.Select(c => c.FileName)));
-
-            var sizeFiles = fileFilter.FilterBySizeRange(fileItems, 100, 4000);
-            Console.WriteLine("Meet Size Files: {0}", string.Join(", ", sizeFiles.Select(c => c.FileName)));
-        }
-
-        // After applying Open-Closed Principle
         //static void Main(string[] args)
         //{
         //    var fileItems = CreateData();
 
-        //    After.FileFilter fileFilter = new After.FileFilter();
+        //    Before.FileFilter fileFilter = new Before.FileFilter();
 
-        //    var results = fileFilter.Filter(fileItems, new After.FilterCriterias.ImageFileCriteriaFilter());
-        //    Console.WriteLine("Image Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+        //    var imageFiles = fileFilter.FilterByType(fileItems, FileType.Image);
+        //    Console.WriteLine("Image Files: {0}", string.Join(", ", imageFiles.Select(c => c.FileName)));
 
-        //    results = fileFilter.Filter(fileItems, new After.FilterCriterias.FileNameCriteriaFilter("File_3"));
-        //    Console.WriteLine("Image Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+        //    var latestFiles = fileFilter.FilterByName(fileItems, "File_3");
+        //    Console.WriteLine("Latest Files: {0}", string.Join(", ", latestFiles.Select(c => c.FileName)));
 
-        //    results = fileFilter.Filter(fileItems, new After.FilterCriterias.FileSizeRangeCriteriaFilter(100, 4000));
-        //    Console.WriteLine("Meet Size Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+        //    var sizeFiles = fileFilter.FilterBySizeRange(fileItems, 100, 4000);
+        //    Console.WriteLine("Meet Size Files: {0}", string.Join(", ", sizeFiles.Select(c => c.FileName)));
         //}
+
+        // After applying Open-Closed Principle
+        static void Main(string[] args)
+        {
+            var fileItems = CreateData();
+
+            After.FileFilter fileFilter = new After.FileFilter();
+
+            var results = fileFilter.Filter(fileItems, new After.FilterCriterias.ImageFileCriteriaFilter());
+            Console.WriteLine("Image Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+
+            results = fileFilter.Filter(fileItems, new After.FilterCriterias.FileNameCriteriaFilter("File_3"));
+            Console.WriteLine("Image Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+
+            results = fileFilter.Filter(fileItems, new After.FilterCriterias.FileSizeRangeCriteriaFilter(100, 4000));
+            Console.WriteLine("Meet Size Files: {0}", string.Join(", ", results.Select(c => c.FileName)));
+        }
 
         private static IEnumerable<FileItem> CreateData()
         {
